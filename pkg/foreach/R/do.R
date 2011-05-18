15f0daaa1c8069c2421e2995b77b9e2b16bc3e57
weston <stephen.b.weston@gmail.com>
2011-05-18 11:56:22 +0000

    Added comment about the expression compilation.

--- pkg/foreach/R/do.R
+++ pkg/foreach/R/do.R
@@ -112,6 +112,7 @@ doSEQ <- function(obj, expr, envir, data) {
   for (p in obj$packages)
     library(p, character.only=TRUE)
 
+  # compile the expression if we're using R 2.13.0 or greater
   xpr <- comp(expr, env=envir, options=list(suppressUndefined=TRUE))
 
   i <- 1
