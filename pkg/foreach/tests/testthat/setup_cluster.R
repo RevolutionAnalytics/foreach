cd13852c726d52e193bfa088117b6d233fd226bb
Hong Ooi <hongooi@microsoft.com>
2020-02-04 04:40:16 +1100

    fixes for R cmd check

--- pkg/foreach/tests/testthat/setup_cluster.R
+++ pkg/foreach/tests/testthat/setup_cluster.R
@@ -1,20 +1,11 @@
 method <- Sys.getenv("FOREACH_BACKEND", "SEQ")
 
-if(method == "SNOW") {
-  cl <- snow::makeSOCKcluster(2)
-  .Last <- function() {
-    snow::stopCluster(cl)
-  }
-  doSNOW::registerDoSNOW(cl)
-} else if(method == "PAR") {
+if(method == "PAR") {
   cl <- parallel::makeCluster(2, type="PSOCK")
   .Last <- function() {
     parallel::stopCluster(cl)
   }
   doParallel::registerDoParallel(cl)
-# } else if(method == 'NWS') {
-#   library(doNWS)
-#   registerDoNWS()
 } else if(method == 'MC') {
   doMC::registerDoMC()
 } else if(method == 'SEQ') {
